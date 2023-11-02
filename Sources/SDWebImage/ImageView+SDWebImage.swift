e795e16c7a3170196de270423c161fa0a830f178
yun <lixingyunxing@163.com>
2023-11-02 10:46:22 +0800

    sdwebimage retryFailed

--- Sources/SDWebImage/ImageView+SDWebImage.swift
+++ Sources/SDWebImage/ImageView+SDWebImage.swift
@@ -13,7 +13,8 @@ public typealias SDWebImageProgress = (Int,Int,URL?) -> Void
 
 extension ImageView {
         
-    public convenience init(url: String, placeholderImage: UIImage? = nil, options:SDWebImageOptions = .waitStoreCache,context: [SDWebImageContextOption : Any]? = nil, progress: SDWebImageProgress? = nil, completed: SDWebImageCompleted? = nil ){
+    public convenience init(url: String, placeholderImage: UIImage? = nil, options:SDWebImageOptions = .retryFailed,context: [SDWebImageContextOption : Any]? = nil, progress: SDWebImageProgress? = nil, completed: SDWebImageCompleted? = nil ){
+        print(options)
         self.init()
         self.flex.markDirty()
         self.sd_setImage(with: URL(string: url), placeholderImage: placeholderImage, options: options, context: context,progress: progress,completed: completed)
